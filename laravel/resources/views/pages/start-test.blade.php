1e348c13146765d5ae27963a6b8b0f9f4ee5ee4a
Gbzzz <gbp3@aluno.ifal.edu.br>
2023-06-06 12:01:53 -0300

    update contador

--- laravel/resources/views/pages/start-test.blade.php
+++ laravel/resources/views/pages/start-test.blade.php
@@ -152,22 +152,28 @@
 @push('js')
     <script>
         function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
+        var timer = duration, hours, minutes, seconds;
         setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
+            hours = parseInt(timer / 3600, 10);
+            minutes = parseInt((timer % 3600) / 60, 10);
             seconds = parseInt(timer % 60, 10);
+
+            hours = hours < 10 ? "0" + hours : hours;
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
-            display.textContent = minutes + ":" + seconds;
+
+            display.textContent = hours + ":" + minutes + ":" + seconds;
+
             if (--timer < 0) {
                 timer = duration;
             }
         }, 1000);
     }
+
     window.onload = function () {
-        var duration = parseFloat("<?=$start_tests;?>");
-            display = document.querySelector('#timer'); // selecionando o timer
-        startTimer(duration, display); // iniciando o timer
+        var duration = parseInt("<?=$start_tests;?>");
+        display = document.querySelector('#timer');
+        startTimer(duration, display);
     };
     </script>
 @endpush
